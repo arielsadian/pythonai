9a89694e4a4e4dd42d59752c2ca09d1b1287b000
arielsadian <hg8hzhymbg@privaterelay.appleid.com>
2026-07-25 13:24:28 +0300

    Add files via upload

---
+++ targil for.py
@@ -0,0 +1,12 @@
+#start
+height = int(input("height"))
+for i in range(1, height + 1):
+    for j in range (i):
+        print('*',end=' ')
+    print()
+
+
+for i in range(height,0 , -1):
+    for j in range (i):
+      print('*', end=' ')
+    print()
